f350372a7d7ce320e789ed1a90a471805f04e903
Z-Git <152425632+DJPZ3110@users.noreply.github.com>
2026-06-02 21:37:07 +0100

    Update Appwrite project ID in config.js

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,6 +1,6 @@
 
 const appwriteConfig = {
     endpoint: 'https://cloud.appwrite.io/v1',
-    projectId: 'VOTRE_PROJECT_ID'   // ← Remplacez par l'ID de votre projet Appwrite
+    projectId: 'texlo'   // ← Remplacez par l'ID de votre projet Appwrite
 };
 export default appwriteConfig;
